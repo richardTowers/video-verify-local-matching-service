b477435a7fc415cd1573e433d3a4acaab33ac8e0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-05-16 12:41:59 +0000

    Initial commit

---
+++ Controllers/LocalMatchingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace local_matching_service.Controllers
+{
+    [Route("local-matching")]
+    public class LocalMatchingController : Controller
+    {
+        private Random rand = new Random();
+
+        [HttpPost("match")]
+        public dynamic Match()
+        {
+            if (rand.Next(2) == 1) {
+                return new { result = "match" };
+            }
+            return new { result = "no-match" };
+        }
+    }
+}
